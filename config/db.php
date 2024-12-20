3083767c8047ca8cb93ed512a290a3652121a230
Юрій Боровський <yurgenoa@MacBook-Air-Urij.local>
2024-12-20 11:07:37 +0200

    db user changed

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=logistic_crm',
-    'username' => 'admin',
+    'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
 
